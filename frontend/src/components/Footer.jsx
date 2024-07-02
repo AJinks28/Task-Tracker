d73943e1f96880e3395dfcc96990c69c09115304
AJinks28 <aekadam2002@gmail.com>
2024-07-02 23:32:56 +0530

    commit frontend

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <p>Made by <a href='https://github.com/AJinks28' target='_blank' rel="noreferrer" className='link'>Ajinkya K</a></p>
+    </div>
+  )
+}
+
+export default Footer
